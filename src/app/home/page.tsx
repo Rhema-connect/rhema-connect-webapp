c59beb8ccb8ba0a55e079fd0fce2448d141784f5
daniel <danielemmanuel257@gmail.com>
2024-01-09 12:20:38 +0100

    initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="w-full h-screen flex items-center ustify-center">
+        <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Home
